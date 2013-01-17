51b767768e95f9b7b1d7855f1e95845bcce287dd
Mir Shahidul Islam <mir.shahidul@gmail.com>
2013-01-17 12:57:06 +0100

    my first commit in git fro experiment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Code::Application.config.secret_token = 'dc1596c169f35fc81b42da98c32ba52f6d8a375a6a431ffa53ed3c5926f2f8d0ea3015612bd0d3d8a2c97fedb9be6b4fe5e7054b045038dcc5a91316b880fe7c'
